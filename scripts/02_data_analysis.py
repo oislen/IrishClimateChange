9bc5807e02452e6b31814b32a24b5812c80bb680
Oisin <oisin.leonard@gmail.com>
2022-10-09 11:55:04 +0100

    Added for loop to load seperate weather data files, and plotting new overlay line plot

--- scripts/02_data_analysis.py
+++ scripts/02_data_analysis.py
@@ -7,18 +7,23 @@ from utilities.time_plot import time_plot
 
 # load dublin airport weather data
 dtypes = {'date':'str'}
-parse_dates = ['date']
-data = pd.read_csv(cons.dublin_airport_fpath, skiprows = 25, dtype = dtypes, parse_dates = parse_dates, na_values = [' '])
+data = pd.DataFrame()
+for fpath in cons.met_eireann_fpaths:
+    print(fpath)
+    tmp_data = pd.read_excel(fpath, dtype = dtypes, na_values = [' '])
+    data = pd.concat(objs = [data, tmp_data], ignore_index = True, axis = 0)
+data['date'] = pd.to_datetime(data['date'])
 
 # aggregate by year month
-agg_data = data.assign(year = data['date'].dt.strftime('%Y'))
-group_cols = ['year']
+agg_data = data.assign(year = data['date'].dt.year)
+group_cols = ['county', 'year']
 agg_dict = {'maxtp':'mean', 'mintp':'mean', 'wdsp':'mean', 'sun':'mean', 'evap':'mean'}
 agg_data = agg_data[agg_data['year'] != '2022'].groupby(group_cols, as_index = False).agg(agg_dict).reset_index()
+#agg_data.pivot(index = ['county', 'year'], columns = ['maxtp', 'mintp', 'wdsp', 'sun', 'evap'])
 
 # generate time plots
-time_plot(agg_data, y = 'maxtp')
-time_plot(agg_data, y = 'mintp')
-time_plot(agg_data, y = 'wdsp')
-time_plot(agg_data, y = 'sun')
-time_plot(agg_data, y = 'evap')
+time_plot(agg_data, x = 'year', y = 'maxtp', hue = 'county', time_span = [2010, 2021], counties = ['dublin', 'wexford'])
+time_plot(agg_data, x = 'year', y = 'mintp', hue = 'county', time_span = [2010, 2021], counties = ['dublin', 'wexford'])
+time_plot(agg_data, x = 'year', y = 'wdsp', hue = 'county', time_span = [2010, 2021], counties = ['dublin', 'wexford'])
+time_plot(agg_data, x = 'year', y = 'sun', hue = 'county', time_span = [2010, 2021], counties = ['dublin', 'wexford'])
+time_plot(agg_data, x = 'year', y = 'evap', hue = 'county', time_span = [2010, 2021], counties = ['dublin', 'wexford'])
