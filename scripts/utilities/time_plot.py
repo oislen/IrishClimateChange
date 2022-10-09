e7c4ddc5fac08e89165ccdc3558e3a83bda3d054
Oisin <oisin.leonard@gmail.com>
2022-10-09 11:53:22 +0100

    Added functionality to filter data with respect to time_span and counties

--- scripts/utilities/time_plot.py
+++ scripts/utilities/time_plot.py
@@ -1,10 +1,16 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-def time_plot(data, y, x = 'index'):
+def time_plot(data, y, x, hue = None, time_span = None, counties = None):
+    # if filtering date with respect to timespan
+    if time_span != None:
+        data = data.loc[(data[x] >= min(time_span)) & (data[x] <= max(time_span)), :]
+    # if filtering data with respect to countues
+    if counties != None:
+        data = data.loc[data[hue].isin(counties)]
     # plot line chart of average max temperature
     sns.set(rc={'figure.figsize':(11.7,8.27)})
-    chart = sns.lineplot(x = x, y = y, data = data)
+    chart = sns.lineplot(x = x, y = y, hue = hue, data = data)
     #chart.set(title = 'Mean Max Temperature', xlabel = 'Year', ylabel = 'Mean Max Temperature')
     # set x axis ticks and labels
     chart.set_xticks(data[x])
