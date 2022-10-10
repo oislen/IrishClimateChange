402984b4d9ae170c96fae8d1c707ec45457205f4
Oisin <oisin.leonard@gmail.com>
2022-10-10 11:51:18 +0100

    Updated analysis script

--- scripts/02_data_analysis.py
+++ scripts/02_data_analysis.py
@@ -3,27 +3,30 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+from utilities.gen_master_data import gen_master_data
+from utilities.time_data import time_data
 from utilities.time_plot import time_plot
 
-# load dublin airport weather data
-dtypes = {'date':'str'}
-data = pd.DataFrame()
-for fpath in cons.met_eireann_fpaths:
-    print(fpath)
-    tmp_data = pd.read_excel(fpath, dtype = dtypes, na_values = [' '])
-    data = pd.concat(objs = [data, tmp_data], ignore_index = True, axis = 0)
-data['date'] = pd.to_datetime(data['date'])
+gen_master_data(cons.met_eireann_fpaths, cons.master_data_fpath)
+
+### Proto-type Bokeh App Plot Design using Seaborn
+
+# load master data
+data = pd.read_feather(cons.master_data_fpath)
 
 # aggregate by year month
-agg_data = data.assign(year = data['date'].dt.year)
-group_cols = ['county', 'year']
-agg_dict = {'maxtp':'mean', 'mintp':'mean', 'wdsp':'mean', 'sun':'mean', 'evap':'mean'}
-agg_data = agg_data[agg_data['year'] != '2022'].groupby(group_cols, as_index = False).agg(agg_dict).reset_index()
-#agg_data.pivot(index = ['county', 'year'], columns = ['maxtp', 'mintp', 'wdsp', 'sun', 'evap'])
+date_strftime_dict = {'year':'%Y', 'year-month':'%Y-%m', 'month':'%m'}
+date_strftime = date_strftime_dict['year']
+time_span = ['2010', '2020']
+counties = ['dublin', 'wexford']
+agg_dict = {'maxtp':'mean', 'mintp':'mean', 'wdsp':'mean', 'sun':'mean', 'evap':'mean', 'rain':'mean'}
+# generate time data
+agg_data = time_data(data = data, agg_dict = agg_dict, time_span = time_span, counties = counties, strftime = date_strftime)
 
 # generate time plots
-time_plot(agg_data, x = 'year', y = 'maxtp', hue = 'county', time_span = [2010, 2021], counties = ['dublin', 'wexford'])
-time_plot(agg_data, x = 'year', y = 'mintp', hue = 'county', time_span = [2010, 2021], counties = ['dublin', 'wexford'])
-time_plot(agg_data, x = 'year', y = 'wdsp', hue = 'county', time_span = [2010, 2021], counties = ['dublin', 'wexford'])
-time_plot(agg_data, x = 'year', y = 'sun', hue = 'county', time_span = [2010, 2021], counties = ['dublin', 'wexford'])
-time_plot(agg_data, x = 'year', y = 'evap', hue = 'county', time_span = [2010, 2021], counties = ['dublin', 'wexford'])
+time_plot(agg_data, x = 'date', y = 'maxtp', hue = 'county', strftime = date_strftime)
+time_plot(agg_data, x = 'date', y = 'mintp', hue = 'county', strftime = date_strftime)
+time_plot(agg_data, x = 'date', y = 'wdsp', hue = 'county', strftime = date_strftime)
+time_plot(agg_data, x = 'date', y = 'sun', hue = 'county', strftime = date_strftime)
+time_plot(agg_data, x = 'date', y = 'evap', hue = 'county', strftime = date_strftime)
+time_plot(agg_data, x = 'date', y = 'rain', hue = 'county', strftime = date_strftime)
