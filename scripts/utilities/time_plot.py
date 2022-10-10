0d432abdf2e381743d9ea25f2b03873d2ab0ac13
Oisin <oisin.leonard@gmail.com>
2022-10-10 11:49:38 +0100

    Generalised time_plot application

--- scripts/utilities/time_plot.py
+++ scripts/utilities/time_plot.py
@@ -1,20 +1,17 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-def time_plot(data, y, x, hue = None, time_span = None, counties = None):
-    # if filtering date with respect to timespan
-    if time_span != None:
-        data = data.loc[(data[x] >= min(time_span)) & (data[x] <= max(time_span)), :]
-    # if filtering data with respect to countues
-    if counties != None:
-        data = data.loc[data[hue].isin(counties)]
-    # plot line chart of average max temperature
+def time_plot(data, y, x, hue = None, strftime = None):
+    """"""
+    tmp_data = data.copy()
     sns.set(rc={'figure.figsize':(11.7,8.27)})
-    chart = sns.lineplot(x = x, y = y, hue = hue, data = data)
-    #chart.set(title = 'Mean Max Temperature', xlabel = 'Year', ylabel = 'Mean Max Temperature')
+    fig, ax = plt.subplots()
+    chart = sns.lineplot(x = x, y = y, hue = hue, data = tmp_data, ax = ax)
     # set x axis ticks and labels
-    chart.set_xticks(data[x])
-    chart.set_xticklabels(data['year'], rotation = 45, size = 5)
+    chart.set_xticks(tmp_data[x])
+    if strftime != None:
+        tmp_data[x] = tmp_data[x].dt.strftime(strftime)
+    chart.set_xticklabels(tmp_data[x], rotation = 45)
     plt.show()
     plt.close()
     return 0
