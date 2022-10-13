4cd7a60e7b43ee3757b6ad81a49745405cfd1166
Oisin <oisin.leonard@gmail.com>
2022-10-13 21:36:00 +0100

    #3 extract out bokeh data and plot generation functions, and intregated callbacks to work with aggregation level

--- scripts/bokeh_app.py
+++ scripts/bokeh_app.py
@@ -9,94 +9,42 @@ Created on Thu Jun 23 11:45:02 2022
 
 # load relevant libraries
 from bokeh.io import curdoc
-from bokeh.models import ColorBar, Select, ColumnDataSource, HoverTool, LinearColorMapper, Panel, Tabs, Span
+from bokeh.models import Select, Panel, Tabs
 from bokeh.layouts import column, row
-from bokeh.palettes import brewer
-from bokeh.plotting import figure
-import math
-import cons
 import pandas as pd
 
-from utilities.time_data import time_data
-
-#target_value = cons.default_select
+# import custom modules
+import cons
+from utilities.bokeh_data import bokeh_data
+from utilities.bokeh_plot import bokeh_plot
 
 # load input datasets
 data = pd.read_feather(cons.master_data_fpath)
-# generate time data aggregated by year
-date_strftime_dict = {'year':'%Y', 'year-month':'%Y-%m', 'month':'%m'}
-counties = ['dublin', 'wexford']
-agg_dict = {'maxtp':'mean', 'mintp':'mean', 'wdsp':'mean', 'sun':'mean', 'evap':'mean', 'rain':'mean'}
-year_strftime = date_strftime_dict['year']
-time_span = ['2010', '2019']
-year_data = time_data(data = data, agg_dict = agg_dict, time_span = time_span, counties = counties, strftime = year_strftime)
-dublin_data = year_data.loc[year_data['county'] == 'dublin', :]
-wexford_data = year_data.loc[year_data['county'] == 'wexford', :]
-# create bokeh data source
-dublin_datasource = ColumnDataSource(dublin_data)
-wexford_datasource = ColumnDataSource(wexford_data)
 
-#################
-#-- Bar Chart --#
-#################
+# generate bokeh data for plot
+bokeh_data_dict = bokeh_data(data = data, option = cons.agg_level_default)
 
-plot = figure(title="Line Plot", toolbar_location='below', output_backend="webgl", **cons.FIG_SETTING)
-dublin_line = plot.line(x='date', y='maxtp', source = dublin_datasource)
-wexford_line = plot.line(x='date', y='maxtp', source = wexford_datasource)
-#plot2.x_range.min_interval = 1
-#plot2.x_range.max_interval = 47.5
-#plot2.xgrid.grid_line_color = None
-#plot2.title.text_font_style="bold"
-#plot2.title.text_font_size="22px"
-#plot2.xaxis.major_label_text_font_size = "11pt"
-#plot2.yaxis.major_label_text_font_size = "11pt"
-#plot2.xaxis.major_label_orientation = math.pi/4
+# create bokeh plot
+plot = bokeh_plot(bokeh_data_dict)
 
-# create hover tools
-plot.add_tools(HoverTool(renderers=[dublin_line],
-                      tooltips=[('Date', '@date'),
-                                ('Max Temperature', '@maxtp')
-                                ],
-                      attachment='left')
-            )
+# create call back function for bokeh dashboard interaction
+def callback(attr, old, new):
+    # extract new selector value
+    target_value = agg_level_selector.value
+    # update bokeh data
+    bokeh_data_dict = bokeh_data(data, option = target_value)
+    # update bokeh plot
+    update_plot = bokeh_plot(bokeh_data_dict)
+    # reassign bokeh plot to bokeh dashboard
+    dashboard.children[0] = update_plot
 
-plot.add_tools(HoverTool(renderers=[wexford_line],
-                      tooltips=[('Date', '@date'),
-                                ('Max Temperature', '@maxtp')
-                                ],
-                      attachment='left')
-            )
+# set up selectors for bokeh dashboard
+agg_level_selector = Select(title='Aggregate Level', value='year', options=cons.agg_level_options, width=300, height=60, aspect_ratio=10)
+agg_level_selector.on_change('value', callback)  
 
-#####################
-#-- App Structure --#
-#####################
-
-# define a blue color palette
-#palette = brewer['Blues'][8]
-# reverse order of colors so higher values have darker colors
-#palette = palette[::-1]  
-# instantiate LinearColorMapper that linearly maps numbers in a range, into a sequence of colors.
-#color_mapper = LinearColorMapper(palette=palette)
-# create color bar.
-#color_bar = ColorBar(color_mapper=color_mapper, label_standoff=8, width=500, height=25, border_line_color=None, location=(0, 0), orientation='horizontal', major_label_text_font_size="18px")
-
-#def callback(attr, old, new):
-#    target_value = selector.value
-#    df1_mod, df2_mod = utils.update_dataframes(legal_level = target_value, df1 = s_22mr22_nonmiss, df2 = ratenorms_latlng_gpd)
-#    df2_mod = df2_mod.sort_values(by = ['target_col'], ascending = False).reset_index(drop = True)
-#    nonmissgeosource.geojson = df1_mod.to_json()
-#    pointsource.geojson = df2_mod.to_json()
-#    plot2.x_range.factors = df2_mod['city'].to_list()
-#    hline.location = df2_mod['target_col'].mean()
-    
-#options=list(cons.selector_map.keys())
-selector = Select(title='Aggregate Level', value='year', options=['year', 'month'], width=300, height=60, aspect_ratio=10)
-#selector.on_change('value', callback)  
-
-# structure app layout
-structure2 = column(plot)
-panel2 = Panel(child = structure2, title = 'Comparison Bar Chart')
-
-tab2 = Tabs(tabs=[panel2])
-layout = column(row(tab2), selector)
+# structure dashboard layout
+dashboard = column(plot)
+panel = Panel(child = dashboard, title = 'Irish Climate Change')
+tab = Tabs(tabs=[panel])
+layout = column(row(tab), agg_level_selector)
 curdoc().add_root(layout)
