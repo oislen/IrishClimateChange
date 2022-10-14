8563f9c324fccb82491668f6cea53662c404ae14
Oisin <oisin.leonard@gmail.com>
2022-10-14 15:58:28 +0100

    #3 Created and integrated a gis map of Irish climate with the existing time series plot

--- scripts/bokeh_app.py
+++ scripts/bokeh_app.py
@@ -12,39 +12,52 @@ from bokeh.io import curdoc
 from bokeh.models import Select, Panel, Tabs
 from bokeh.layouts import column, row
 import pandas as pd
+import geopandas as gpd
 
 # import custom modules
 import cons
-from utilities.bokeh_data import bokeh_data
-from utilities.bokeh_plot import bokeh_plot
+from utilities.bokeh_map_data import bokeh_map_data
+from utilities.bokeh_map_plot import bokeh_map_plot
+from utilities.bokeh_line_data import bokeh_line_data
+from utilities.bokeh_line_plot import bokeh_line_plot
 
 # load input datasets
 data = pd.read_feather(cons.master_data_fpath)
+counties = gpd.read_file(cons.counties_data_fpath)
 
-# generate bokeh data for plot
-bokeh_data_dict = bokeh_data(data = data, option = cons.agg_level_default)
+# generate bokeh data for map plot
+bokeh_map_data_dict = bokeh_map_data(data, counties)
+# create bokeh map plot
+map_plot = bokeh_map_plot(bokeh_map_data_dict)
 
+# generate bokeh data for line plot
+bokeh_line_data_dict = bokeh_line_data(data = data, option = cons.agg_level_default)
 # create bokeh plot
-plot = bokeh_plot(bokeh_data_dict)
+line_plot = bokeh_line_plot(bokeh_line_data_dict)
 
 # create call back function for bokeh dashboard interaction
-def callback(attr, old, new):
+def callback_line_plot(attr, old, new):
     # extract new selector value
     target_value = agg_level_selector.value
     # update bokeh data
-    bokeh_data_dict = bokeh_data(data, option = target_value)
+    bokeh_line_data_dict = bokeh_line_data(data, option = target_value)
     # update bokeh plot
-    update_plot = bokeh_plot(bokeh_data_dict)
+    line_plot = bokeh_line_plot(bokeh_line_data_dict)
     # reassign bokeh plot to bokeh dashboard
-    dashboard.children[0] = update_plot
+    dashboard_line.children[0] = line_plot
 
 # set up selectors for bokeh dashboard
 agg_level_selector = Select(title='Aggregate Level', value='year', options=cons.agg_level_options, width=300, height=60, aspect_ratio=10)
-agg_level_selector.on_change('value', callback)  
+agg_level_selector.on_change('value', callback_line_plot)  
 
-# structure dashboard layout
-dashboard = column(plot)
-panel = Panel(child = dashboard, title = 'Irish Climate Change')
-tab = Tabs(tabs=[panel])
-layout = column(row(tab), agg_level_selector)
+# structure dashboard map plot
+dashboard_map = column(map_plot)
+panel_map = Panel(child = dashboard_map, title = 'GIS Map')
+tab_map = Tabs(tabs=[panel_map])
+# structure dashboard line plot
+dashboard_line = column(line_plot, agg_level_selector)
+panel_line = Panel(child = dashboard_line, title = 'Time Series')
+tab_line = Tabs(tabs=[panel_line])
+# combine map and line dashboards
+layout = column(row(tab_map, tab_line))
 curdoc().add_root(layout)
