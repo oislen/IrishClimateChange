80f372395ea456fefada44d0ac4dac5828bb2384
Oisin <oisin.leonard@gmail.com>
2022-10-16 21:31:52 +0100

    #2 Added gen_map_data.py to preprocessing_data.py script, and regenerated data

--- scripts/preprocess_data.py
+++ scripts/preprocess_data.py
@@ -1,5 +1,6 @@
 from utilities.gen_master_data import gen_master_data
 from utilities.preaggregate_data import preaggregate_data
+from utilities.gen_counties_data import gen_counties_data
 from utilities.bokeh_line_data import bokeh_line_data
 from utilities.bokeh_map_data import bokeh_map_data
 
@@ -8,3 +9,5 @@ if __name__ == '__main__':
     gen_master_data()
     # generate the preaggregate data
     preaggregate_data()
+    # generate counties data
+    gen_counties_data()
