06037e8d18bdb288fea6372dbd2ede9b266e01ac
Oisin <oisin.leonard@gmail.com>
2022-10-17 10:12:10 +0100

    #3 Moved preprocessing data scripts to seperate directory

--- scripts/preprocess_data.py
+++ scripts/preprocess_data.py
@@ -1,8 +1,6 @@
-from utilities.gen_master_data import gen_master_data
-from utilities.preaggregate_data import preaggregate_data
-from utilities.gen_counties_data import gen_counties_data
-from utilities.bokeh_line_data import bokeh_line_data
-from utilities.bokeh_map_data import bokeh_map_data
+from PreProcessData.gen_master_data import gen_master_data
+from PreProcessData.preaggregate_data import preaggregate_data
+from PreProcessData.gen_counties_data import gen_counties_data
 
 if __name__ == '__main__':
     # generate master data file
