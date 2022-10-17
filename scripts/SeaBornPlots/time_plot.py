477f007825cef4519ada7a1c21e21f2478923d04
Oisin <oisin.leonard@gmail.com>
2022-10-17 14:28:54 +0100

    #1 Changed from sns.lineplot to sns.pointplot, and moved legend to rightside of plot

--- scripts/SeaBornPlots/time_plot.py
+++ scripts/SeaBornPlots/time_plot.py
@@ -11,22 +11,26 @@ def time_plot(data, y, x = 'index', hue = None, strftime = None, refline = None,
     sns.set_style("white")
     # initiate subplots
     fig, ax = plt.subplots()
+    # create point plot
+    line_chart = sns.pointplot(data = tmp_data, x = x, y = y, hue = hue, ax = ax,  palette = cons.county_line_colors)
+    #point_chart.legend_.remove()
     # create line plot
-    chart = sns.lineplot(x = x, y = y, hue = hue, data = tmp_data, ax = ax, palette = cons.county_line_colors)
+    #line_chart = sns.lineplot(data = tmp_data, x = x, y = y, hue = hue, palette = cons.county_line_colors)
+    sns.move_legend(ax, "upper left", bbox_to_anchor=(1, 1))
     # add horizontal reference line
     if refline != None:
         plt.axhline(y = refline, color = 'red', linestyle = '--')
     # set x axis ticks and labels
     axis_data_dict = tmp_data[['index', 'date_str']].drop_duplicates().sort_values(by = 'index').set_index('index').to_dict()['date_str']
-    chart.set_xticks(list(axis_data_dict.keys()))
-    chart.set_xticklabels(list(axis_data_dict.values()), rotation = 45)
+    line_chart.set_xticks(list(axis_data_dict.keys()))
+    line_chart.set_xticklabels(list(axis_data_dict.values()), rotation = 45)
     # add title and axis labels to plot
     if title != None:
-        chart.set(title = title)
+        line_chart.set(title = title)
     if xlabel != None:
-        chart.set(xlabel = xlabel)
+        line_chart.set(xlabel = xlabel)
     if ylabel != None:
-        chart.set(ylabel = ylabel)
+        line_chart.set(ylabel = ylabel)
     # show and close plot
     plt.show()
     plt.close()
