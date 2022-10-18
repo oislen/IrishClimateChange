abf91309ff5e14aeda1b8a10c022a08af583a6f6
Oisin <oisin.leonard@gmail.com>
2022-10-18 09:25:44 +0100

    #1 automated root_dir assignmation based on platform os system

--- scripts/cons.py
+++ scripts/cons.py
@@ -1,21 +1,21 @@
 # load relevant libraries
 import os
+import platform
 
+root_dir = 'E:\\GitHub\\IrishClimateApp' if platform.system() == 'Windows' else '~/IrishClimateApp'
 # set directories
-root_dir = 'E:\\GitHub\\IrishClimateApp'
 data_dir = os.path.join(root_dir, 'data')
 census_2011_dir = os.path.join(data_dir, 'Census_2011')
 census_2016_dir = os.path.join(data_dir, 'Census_2016')
 met_eireann_dir = os.path.join(data_dir, 'Met_Eireann')
 gis_dir = os.path.join(data_dir, "gis")
-
 # set data files
 master_data_fpath = os.path.join(data_dir, 'master.feather')
 preaggregate_data_fpath = os.path.join(data_dir, "preaggregate_data.pickle")
 bokeh_line_data_fpath = os.path.join(data_dir, "bokeh_line_data.pickle")
 bokeh_map_data_fpath = os.path.join(data_dir, "bokeh_map_data.pickle")
-rep_counties_fpath = os.path.join(data_dir, 'gis\\arch\\Counties_-_OSi_National_Statutory_Boundaries_-_2019\\Counties___OSi_National_Statutory_Boundaries_.shp')
-ni_counties_fpath = os.path.join(data_dir, 'gis\\arch\\northern_ireland_counties\\northern_ireland_counties.shp')
+rep_counties_fpath = os.path.join(data_dir, 'gis', 'arch', 'Counties_-_OSi_National_Statutory_Boundaries_-_2019', 'Counties___OSi_National_Statutory_Boundaries_.shp')
+ni_counties_fpath = os.path.join(data_dir, 'gis', 'arch', 'northern_ireland_counties', 'northern_ireland_counties.shp')
 counties_data_fpath = os.path.join(gis_dir, "counties.shp")
 map_data_fpath = os.path.join(gis_dir, "map_data.pickle")
 
