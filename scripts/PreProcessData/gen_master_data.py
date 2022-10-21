d183d75dfee46ea7a5dada6f954e6650dea315b3
Oisin <oisin.leonard@gmail.com>
2022-10-21 15:16:48 +0100

    #2 replaced for loop with list comprehension

--- scripts/PreProcessData/gen_master_data.py
+++ scripts/PreProcessData/gen_master_data.py
@@ -2,26 +2,22 @@ import os
 import cons
 import pandas as pd
 
-def gen_master_data(master_data_fpath = None, return_data = True):
+def gen_master_data(met_eireann_fpaths = None, master_data_fpath = None, return_data = True):
     """"""
-    print('~~~ Generating master data ...')
-    # load data files from file directory
-    met_eireann_fpaths = [os.path.join(cons.met_eireann_dir, fpath) for fpath in os.listdir(cons.met_eireann_dir) if '.xlsx' in fpath]
+    if met_eireann_fpaths == None:
+        # load data files from file directory
+        met_eireann_fpaths = [os.path.join(cons.met_eireann_dir, fpath) for fpath in os.listdir(cons.met_eireann_dir) if '.xlsx' in fpath]
     dtypes = {'date':'str'}
-    data = pd.DataFrame()
-    for fpath in met_eireann_fpaths:
-        print(f'loading {fpath} ...')
-        tmp_data = pd.read_excel(fpath, dtype = dtypes, na_values = [' '])
-        tmp_data_sub = tmp_data[tmp_data.columns[~tmp_data.columns.str.contains('ind')]]
-        data = pd.concat(objs = [data, tmp_data_sub], ignore_index = True, axis = 0)
+    data_list = [pd.read_excel(fpath, dtype = dtypes, na_values = [' ']) for fpath in met_eireann_fpaths]
+    data = pd.concat(objs = data_list, ignore_index = True, axis = 0)
+    data = data[data.columns[~data.columns.str.contains('ind')]]
     data['date'] = pd.to_datetime(data['date'])
     # order results by county and station alphabetically
     data = data.sort_values(by = ['county', 'station']).reset_index(drop = True)
     # if the output
     if master_data_fpath != None:
         if os.path.exists(master_data_fpath):
-            # save concatenated data to disk and reread
-            print('outputting feather file ...')
+            # save concatenated data to disk
             data.to_feather(master_data_fpath)
         else:
             raise ValueError(f'{master_data_fpath} does not exist')
