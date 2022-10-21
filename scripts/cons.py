ef0cf8ade23c9209c3e3b8f1363a1b26e825dd9e
Oisin <oisin.leonard@gmail.com>
2022-10-21 10:30:34 +0100

    #2 Added data files for donegal

--- scripts/cons.py
+++ scripts/cons.py
@@ -42,7 +42,7 @@ MAP_SETTINGS = {'line_color':'gray',
                 }
 
 # bokeh line selector settings
-county_line_colors = {'dublin':'blue', 'cork':'red', 'galway':'maroon', 'kerry':'gold', 'kilkenny':'yellow', 'mayo':'green', 'wexford':'purple'}
+county_line_colors = {'donegal':'greenyellow', 'dublin':'blue', 'cork':'red', 'galway':'maroon', 'kerry':'gold', 'kilkenny':'yellow', 'mayo':'green', 'wexford':'purple'}
 counties = list(county_line_colors.keys())
 line_colors = list(county_line_colors.values())
 date_strftime_dict = {'year':'%Y', 'year-month':'%Y-%m', 'month':'%m'}
