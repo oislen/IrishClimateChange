350254d4dd32794b85a6d5d804d943e04bf1cdff
Oisin <oisin.leonard@gmail.com>
2023-06-11 16:13:15 +0100

    #6 simplified conda environments generation using requirements.txt

--- conda/irishclimateapp.bat
+++ conda/irishclimateapp.bat
@@ -1,34 +1,14 @@
 :: create and activate new environment
-call conda env remove --name ica
-call conda create --name ica python --yes
-call conda activate ica
+call conda deactivate
+call conda env remove --name irishclimateapp
+call conda create --name irishclimateapp python --yes
+call conda activate irishclimateapp
 
 :: update conda version
 call conda update -n base conda --yes
 
-:: install required data processing packages
-call conda install -c conda-forge numpy --yes
-call conda install -c conda-forge pyarrow --yes
-call conda install -c conda-forge pyexcel --yes
-call conda install -c conda-forge openpyxl --yes
-call conda install -c conda-forge pandas --yes
-call conda install -c conda-forge geopandas --yes
-
-:: install required visualisation packages
-call conda install -c conda-forge colour --yes
-call conda install -c conda-forge matplotlib --yes
-call conda install -c conda-forge seaborn --yes
-call conda install -c conda-forge bokeh --yes
-
-:: install boto3 for aws connection
-call conda install -c conda-forge boto3
-
-:: install jupyterlab
-call conda install -c conda-forge ipywidgets --yes
-call conda install -c conda-forge jupyterlab --yes
+:: install relevant libraries
+call pip install -r ..\requirements.txt
 
 :: export environment to .yml file
-call conda env export > environment.yml
-
-:: deactivate new environment
-call conda deactivate
+call conda env export > irishclimateapp.yml
--- conda/irishclimateapp.sh
+++ conda/irishclimateapp.sh
@@ -1,34 +1,14 @@
 # create and activate new environment
-conda env remove --name ica
-conda create --name ica python --yes
-conda activate ica
+conda deactivate
+conda env remove --name irishclimateapp
+conda create --name irishclimateapp python --yes
+conda activate irishclimateapp
 
 # update conda version
 conda update -n base conda --yes
 
-# install required data processing packages
-conda install -c conda-forge numpy --yes
-conda install -c conda-forge pyarrow --yes
-conda install -c conda-forge pyexcel --yes
-conda install -c conda-forge openpyxl --yes
-conda install -c conda-forge pandas --yes
-conda install -c conda-forge geopandas --yes
-
-# install required visualisation packages
-conda install -c conda-forge colour --yes
-conda install -c conda-forge matplotlib --yes
-conda install -c conda-forge seaborn --yes
-conda install -c conda-forge bokeh --yes
-
-# install boto3 for aws connection
-conda install -c conda-forge boto3
-
-# install jupyterlab
-conda install -c conda-forge ipywidgets --yes
-conda install -c conda-forge jupyterlab --yes
+# install relevant libraries
+pip install -r ../requirements.txt
 
 # export environment to .yml file
-conda env export > environment.yml
-
-# deactivate new environment
-conda deactivate
+conda env export > irishclimateapp.yml
