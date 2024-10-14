bee6fde19fccfb43a8429ee918fcd6a8b5688371
Oisin <oisin.leonard@gmail.com>
2024-10-14 20:44:51 +0100

    Removed dictionary wrapper for counties data

--- dashboard/utilities/bokeh_map_data.py
+++ dashboard/utilities/bokeh_map_data.py
@@ -7,50 +7,58 @@ from beartype import beartype
 
 @beartype
 def bokeh_map_data(
-    map_data_dict:dict, 
-    station_data:gpd.GeoDataFrame
+    map_data:gpd.GeoDataFrame, 
+    station_data:gpd.GeoDataFrame,
+    col:str,
+    stat:str,
+    year:str
     ):
     """Generates the data used in the bokeh map plot.
 
     Parameters
     ----------
-    map_data_dict : dict
+    map_data : gpd.GeoDataFrame
         The aggregated geopandas data to be transformed into aggregated bokeh data objects for visualisation
     station_data : geopandas.DataFrame
         The geospatial Met Eireann station data to be transformed into a bokeh data object for visualisation
+    col : str
+        The climate measure category to plot in the interactive bokeh heatmap
+    stat : str
+        The aggregated statistic to plot in the interactive bokeh heatmap
+    year : str
+        The year of the data to filter for
 
     Returns
     -------
-    dict, bokeh.models.GeoJSONDataSource
+    dict
         The aggregated and station bokeh data objects to visualise
     """
     bokeh_map_data_dict = {}
-    for stat, map_data in map_data_dict.items():
-        tmp_data_dict = {}
-        # split data into missing and nonmissing
-        nonmiss_map_data = map_data[map_data[cons.col_options].notnull().any(axis=1)]
-        miss_map_data = map_data[~map_data[cons.col_options].notnull().any(axis=1)]
-        # Input GeoJSON source that contains features for plotting
-        missgeosource = GeoJSONDataSource(geojson=miss_map_data.to_json())
-        nonmissgeosource = GeoJSONDataSource(geojson=nonmiss_map_data.to_json())
-        ## create filtered column data source views
-        #dataview_dict = {}
-        #for year in cons.linedash_year_options:
-        #    nonmiss_map_dataview = GeoJSONDataSource(geojson=nonmiss_map_data.loc[(nonmiss_map_data['year'] == year), :].to_json())
-        #    miss_map_dataview = GeoJSONDataSource(geojson=miss_map_data.loc[(miss_map_data['year'] == year), :].to_json())
-        #    cfg_dict = {"nonmiss_map_dataview":nonmiss_map_dataview, "miss_map_dataview":miss_map_dataview}
-        #    dataview_dict[year] = cfg_dict
-        # assign data to temp dict
-        tmp_data_dict["map_data"] = map_data
-        tmp_data_dict["nonmiss_map_data"] = nonmiss_map_data
-        tmp_data_dict["miss_map_data"] = miss_map_data
-        tmp_data_dict["missgeosource"] = missgeosource
-        tmp_data_dict["nonmissgeosource"] = nonmissgeosource
-        #tmp_data_dict["dataview_dict"] = dataview_dict
-        # assign temp data dict to bokeh data dict
-        bokeh_map_data_dict[stat] = tmp_data_dict
+    # filter for year
+    row_filter = (map_data['year'].astype(str) == year) & (map_data['stat'].astype(str) == stat)
+    sub_cols = ['county', 'geometry', 'year', 'stat', col]
+    map_data_sub = map_data.loc[row_filter, sub_cols]
+    # split data into missing and nonmissing
+    nonmiss_map_data = map_data_sub[map_data_sub.notnull().all(axis=1)]
+    miss_map_data = map_data_sub[map_data_sub.isnull().any(axis=1)]
+    # Input GeoJSON source that contains features for plotting
+    missgeosource = GeoJSONDataSource(geojson=miss_map_data.to_json())
+    nonmissgeosource = GeoJSONDataSource(geojson=nonmiss_map_data.to_json())
     pointgeosource = GeoJSONDataSource(geojson=station_data.to_json())
+    # calculate colour bar range
+    color_mapper_low = nonmiss_map_data[col].min()
+    color_mapper_high = nonmiss_map_data[col].max()
+    # assign data to temp dict
+    bokeh_map_data_dict["col"] = col
+    bokeh_map_data_dict["map_data"] = map_data
+    bokeh_map_data_dict["nonmiss_map_data"] = nonmiss_map_data
+    bokeh_map_data_dict["miss_map_data"] = miss_map_data
+    bokeh_map_data_dict["missgeosource"] = missgeosource
+    bokeh_map_data_dict["nonmissgeosource"] = nonmissgeosource
+    bokeh_map_data_dict["pointgeosource"] = pointgeosource
+    bokeh_map_data_dict["color_mapper_low"] = color_mapper_low
+    bokeh_map_data_dict["color_mapper_high"] = color_mapper_high
     # pickle the bokeh map data dictionary to disk
     # with open(cons.bokeh_map_data_fpath, 'wb') as f:
     #    pickle.dump(bokeh_map_data_dict, f, protocol = pickle.HIGHEST_PROTOCOL)
-    return bokeh_map_data_dict, pointgeosource
+    return bokeh_map_data_dict
