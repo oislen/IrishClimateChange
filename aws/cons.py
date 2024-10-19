15777047a51ca997e409fdf4027e7607edd89aa6
Oisin <oisin.leonard@gmail.com>
2024-10-19 22:17:58 +0100

    Loading reference aws config files.

--- aws/cons.py
+++ aws/cons.py
@@ -8,6 +8,16 @@ sys.path.append(root_dir)
 # set directories
 data_dir = os.path.join(root_dir, 'data')
 creds_data = os.path.join(root_dir, '.creds')
+ec2_ref_data_dir = os.path.join(data_dir, "ec2", "ref")
 session_token_fpath = os.path.join(creds_data, "sessionToken.json")
+create_launch_template_config_fpath = os.path.join(ec2_ref_data_dir, "create_launch_template_config.json")
+create_fleet_config_fpath = os.path.join(ec2_ref_data_dir, "create_fleet_config.json")
+run_instances_config_fpath = os.path.join(ec2_ref_data_dir, "run_instances_config.json")
 
-# aws ec2 constants
+# load aws ec2 references
+with open(create_launch_template_config_fpath) as json_file: 
+    create_launch_template_config = json.load(json_file)
+with open(create_fleet_config_fpath) as json_file: 
+    create_fleet_config = json.load(json_file)
+with open(run_instances_config_fpath) as json_file: 
+    run_instances_config = json.load(json_file)
