67b16cb9365efc69973b97daa92df0a00f83f3ee
Oisin <oisin.leonard@gmail.com>
2025-01-27 08:14:41 +0000

    Updated map plot to use geopandas map data and filter for year and stat.

--- dashboard/arch/map_plot.py
+++ dashboard/arch/map_plot.py
@@ -6,8 +6,9 @@ from beartype import beartype
 
 @beartype
 def map_plot(
-    map_data_dict:dict, 
-    station_data:gpd.GeoDataFrame, 
+    map_data:gpd.GeoDataFrame, 
+    station_data:gpd.GeoDataFrame,
+    year:str,
     stat:str, 
     col:str
     ) -> int:
@@ -15,10 +16,12 @@ def map_plot(
 
     Parameters
     ----------
-    map_data_dict : dict
-        A dictionary containing the gis map data for each given statistic to plot
+    map_data : geopandas.DataFrame
+        The gis map data for each given statistic to plot
     station_data : geopandas.DataFrame
         The Met Eireann station data to plot as an overlay of red points over the gis map
+    year : str
+        The year to plot the gis heatmap for
     stat : str
         The specific statistic to plot the gis map for
     col : str
@@ -30,17 +33,19 @@ def map_plot(
         Successful execution
     """
     # create map of ireland
-    map_data = map_data_dict[stat]
+    sub_cols = ["county", "geometry", "year", col, "stat"]
+    row_filter = (map_data["year"] == year) & (map_data["stat"] == stat)
+    map_plot_data = map_data.loc[row_filter, sub_cols]
     fig, ax = plt.subplots(figsize=cons.sns_fig_settings["figure.figsize"])
     plt.axis("off")
     plt.title("Irish Climate App", size=20)
     # plot counties of the island of Ireland
-    map_data["geometry"].boundary.plot(ax=ax, color="grey", linewidth=0.9, alpha=0.6)
+    map_plot_data["geometry"].boundary.plot(ax=ax, color="grey", linewidth=0.9, alpha=0.6)
     # create custom colour map
     cmap = matplotlib.colors.LinearSegmentedColormap.from_list(
         "", ["lightblue", "steelblue"]
     )
-    norm = plt.Normalize(map_data[col].min(), map_data[col].max())
+    norm = plt.Normalize(map_plot_data[col].min(), map_plot_data[col].max())
     # plot heatmap of county climate
     legend_kwds = {
         "label": col,
@@ -48,7 +53,7 @@ def map_plot(
         "fraction": 0.046,
         "pad": 0.04,
     }
-    map_data.plot(
+    map_plot_data.plot(
         column=col, ax=ax, cmap=cmap, legend=True, legend_kwds=legend_kwds, norm=norm
     )
     # overlay met eireann weather station locations
