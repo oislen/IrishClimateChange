82dde38b44e661014291d1d51e1056e0a3ddacbb
Oisin <oisin.leonard@gmail.com>
2025-02-03 14:01:48 +0000

    Applying from 2010 data filter to gen master data process. Using master data instead of pre-aggregated data to generation gis map and points data

--- webscraper/utilities/gen_map_data.py
+++ webscraper/utilities/gen_map_data.py
@@ -2,6 +2,8 @@ import cons
 import os
 import logging
 import pickle
+import numpy as np
+import polars as pl
 import pandas as pd
 import geopandas as gpd
 from beartype import beartype
@@ -11,7 +13,7 @@ from typing import Union
 def gen_map_data(
     rep_counties_fpath:str=cons.rep_counties_fpath,
     ni_counties_fpath:str=cons.ni_counties_fpath,
-    preaggregate_data_fpath:str=cons.preaggregate_data_fpath,
+    master_data_fpath:str=cons.master_data_fpath,
     map_data_fpath:str=cons.map_data_fpath
     ):
     """Generates counties map data for the bokeh map dashboard
@@ -22,8 +24,8 @@ def gen_map_data(
         The file path to the republic of ireland counties .shp file on disk, default is cons.rep_counties_fpath,
     ni_counties_fpath : str
         The file path to northern ireland counties .shp file on disk, default is cons.ni_counties_fpath
-    pre_agg_data_dict : str
-        The file path to the pre-aggregated data on disk, default is cons.preaggregate_data_fpath
+    master_data_fpath : str
+        The file path to the pre-aggregated data on disk, default is cons.master_data_fpath
     map_data_fpath : str
         The file location to write the map data to disk, default is map_data_fpath
 
@@ -36,7 +38,7 @@ def gen_map_data(
     ni_counties = gpd.read_file(ni_counties_fpath)[["county", "geometry"]].to_crs(epsg=2157)
     logging.info("Loading pre-aggregated data dictionary ...")
     # load pre-aggregated data
-    pre_agg_data = pd.read_parquet(preaggregate_data_fpath)
+    master_data = pl.read_parquet(master_data_fpath)
     logging.info("Concatenating counties geopandas dataframes ...")
     # concatenate county shape files
     counties = gpd.GeoDataFrame(pd.concat([rep_counties, ni_counties], ignore_index=True), crs="EPSG:2157")
@@ -49,27 +51,23 @@ def gen_map_data(
     logging.info("Ordering results by county name ...")
     # sort data by county
     counties = counties.sort_values(by="county")
-    logging.info("Calculating county level statistics ...")
+    logging.info("Calculating county level aggregate statistics ...")
     # create a dictionary to contain map data
-    map_data_list = []
+    tmp_agg_data_list = []
     # iterate over statistic and pre aggregated data
-    for stat in pre_agg_data["stat"].unique():
+    for stat in cons.stat_options:
         logging.info(f"{stat} ...")
-        tmp_pre_agg_data = pre_agg_data.copy(deep=True)
-        tmp_pre_agg_data['year'] = tmp_pre_agg_data["date"].dt.year.astype(str)
+        tmp_master_data = master_data.clone()
+        tmp_master_data = tmp_master_data.with_columns(pl.col("date").dt.year().cast(pl.String).alias("year"), pl.lit(stat).alias("stat"))
         # aggregate data to county level
-        group_cols = ["county","year"]
-        #group_cols = ["county"]
-        agg_dict = {col: stat for col in cons.col_options}
-        # filter data to be between 2010
-        tmp_pre_agg_data = tmp_pre_agg_data.loc[(tmp_pre_agg_data['year'].astype(int) >= 2010) & (tmp_pre_agg_data['stat'] == stat), :].copy()
-        county_data = tmp_pre_agg_data.groupby(group_cols, as_index=False).agg(agg_dict)
-        county_data['stat'] = stat
-        map_data_list.append(county_data)
-    map_data = pd.concat(objs=map_data_list,axis=0,ignore_index=True)
+        group_cols = ["county","year","stat"]
+        agg_dict = [getattr(pl.col(col), stat)().replace({None:np.nan}).alias(col) for col in cons.col_options]
+        tmp_agg_data = tmp_master_data.group_by(group_cols).agg(agg_dict)
+        tmp_agg_data_list.append(tmp_agg_data)
+    agg_data = pl.concat(items=tmp_agg_data_list,how="vertical")
     # join county level data to map data
     map_geodata = gpd.GeoDataFrame(
-        data=pd.merge(left=counties, right=map_data, on="county", how="left"),
+        data=pd.merge(left=counties, right=agg_data.to_pandas(), on="county", how="left"),
         crs="EPSG:2157",
         )
     if os.path.exists(map_data_fpath):
